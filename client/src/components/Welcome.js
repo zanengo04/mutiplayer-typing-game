cb7f6b99d50d465b601c7d9f8a5091c1e8856d11
Quoc Ngo <zanengo04@gmail.com>
2021-01-25 00:33:18 -0500

    state added to input in welcome screen

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
@@ -13,13 +13,34 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function Welcome() {
     const classes = useStyles();
+    const [username,setUsername] = useState('')
+    const [roomName, setRoomName] = useState('')
+    function handleUsername(e) {
+      setUsername(e.target.value)
+    }
+    function handleRoomName(e) {
+      setRoomName(e.target.value)
+    }
+  console.log(username, roomName)
     return (
         <div>
             <p id='welcome'>Welcome to Typing Game</p>
             <div id = 'formContainer'>
                 <form className={classes.root} noValidate autoComplete="off">
-                    <TextField id="outlined-basic" label="Username" variant="outlined" />
-                    <TextField id="outlined-basic" label="Room Name" variant="outlined" />
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Username" 
+                      variant="outlined" 
+                      value={username}
+                      onChange={handleUsername}
+                    />
+                    <TextField 
+                      id="outlined-basic" 
+                      label="Room Name" 
+                      variant="outlined" 
+                      value={roomName}
+                      onChange={handleRoomName}
+                    />
                 </form>
                 <button><Link to={'/game'}>{'Submit'}</Link></button>
             </div>
